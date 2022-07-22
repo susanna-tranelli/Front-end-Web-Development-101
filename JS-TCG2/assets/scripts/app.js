ba71492a3fdda17a2bce46381d544be70fd0a3d0
susytran <susanna_tranelli@hotmail.it>
2022-07-22 18:03:11 +0200

    wip  exercise

--- JS-TCG2/assets/scripts/app.js
+++ JS-TCG2/assets/scripts/app.js
@@ -31,42 +31,34 @@ function writeToLog(
 
 function calculateResult(calculationType) {
     const enteredNumber = getUserNumberInput();
-    if (
+    const initialResult = currentResult;
+    let mathOperator;
+    if(calculationType === 'ADD'){
+        currentResult += enteredNumber;
+        mathOperator = '+'
+    } else if(calculationType === 'SUBTRACT') {
+        currentResult -= enteredNumber;
+        mathOperator = '-'
+    } else if(calculationType === 'MULTIPLY') {
+        currentResult *= enteredNumber;
+        mathOperator = '*'
+    } else if(calculationType === 'DIVIDE') {
+        currentResult /= enteredNumber;
+        mathOperator = '/'
+    }
+
+    if(
         calculationType !== 'ADD' &&
         calculationType !== 'SUBTRACT' &&
         calculationType !== 'MULTIPLY' &&
-        calculationType !== 'DIVIDE' ||
-        !enteredNumber
+        calculationType !== 'DIVIDE'
     ) {
         return;
     }
 
-    // if (
-    //   calculationType === 'ADD' ||
-    //   calculationType === 'SUBTRACT' ||
-    //   calculationType === 'MULTIPLY' ||
-    //   calculationType === 'DIVIDE'
-    // ) {
-
-    const initialResult = currentResult;
-    let mathOperator;
-    if (calculationType === 'ADD') {
-        currentResult += enteredNumber;
-        mathOperator = '+';
-    } else if (calculationType === 'SUBTRACT') {
-        currentResult -= enteredNumber;
-        mathOperator = '-';
-    } else if (calculationType === 'MULTIPLY') {
-        currentResult *= enteredNumber;
-        mathOperator = '*';
-    } else if (calculationType === 'DIVIDE') {
-        currentResult /= enteredNumber;
-        mathOperator = '/';
-    }
-
     createAndWriteOutput(mathOperator, initialResult, enteredNumber);
     writeToLog(calculationType, initialResult, enteredNumber, currentResult);
-    // }
+
 }
 
 function add() {
